e007ab352d1a353b725eb0c22c302bb7730168d4
Joseph Rohman <josephhrohman@gmail.com>
2019-03-06 12:37:07 -0800

    created game logic 2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,34 @@
+
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = []
-var cardOne = cards[0]
-var cardTwo = cards[2]
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You Found a match!");
-} else {
-alert("Pick Again");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+} 	else {
+	console.log("Sorry, try again.");
+}
 }
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+var flipCard = function(cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You Found a match!");
+}   else {
+	alert("Pick Again");
+}
+}
+
+flipCard(0);
+flipCard(2);
 
