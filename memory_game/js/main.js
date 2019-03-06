5800530b951d65664d0eb46a8b5f0e329e86603d
Joseph Rohman <josephhrohman@gmail.com>
2019-03-06 11:47:51 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,18 @@
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You Found a match!");
+} else {
+alert("Pick Again");
+}
+
+
